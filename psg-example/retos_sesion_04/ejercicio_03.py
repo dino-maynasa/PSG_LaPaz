595fb90b29fb6640e879d163a28eb25d7aa4472a
Dino <dino.maynasa.maydana@gmail.com>
2024-05-24 18:21:17 -0400

    ejercicio_03.py

---
+++ psg-example/retos_sesion_04/ejercicio_03.py
@@ -0,0 +1,6 @@
+#Juego favorito - ajedrez
+print("1. El jugador con las piezas blancas mueven primero")
+print("2. Cada jugador mueve una pieza a la vez")
+print("3. Gana el jugador que hace jaque mate")
+print("4. Ningún jugador debe colocar a su rey en jaque mate de voluntaria")
+print("5. El jugador que realiza el jaque mate debe explicar la situación")
